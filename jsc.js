e4e05d478f48ac07fd0db1095c04825f6b34230d
Pineapple4u <165797715+Pineapple4u@users.noreply.github.com>
2024-09-06 16:40:58 +0530

    Add files via upload

---
+++ jsc.js
@@ -0,0 +1,19 @@
+// string
+var a="this is hw1";
+//number
+var b=555;
+// boolean
+var c= true;
+// null
+var d= null;
+// undefined
+var e = ;
+
+console.log(typeof a)
+console.log(typeof b)
+console.log(typeof c)
+console.log(typeof d)
+console.log(typeof e)
+
+console.log(a, b, c, d, e)
+
